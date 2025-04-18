52e8a715a735f3a168dfdeaa177f2fcb3ff3bc1f
sujeong9928 <tneoddl9928@naver.com>
2025-04-19 04:44:04 +0900

    🔧 Axios인스턴스 수정

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import fetchAdapter from '@vespaiach/axios-fetch-adapter'
 
 const axiosInstance = axios.create({
+  adapter: fetchAdapter,
   baseURL: 'http://localhost:8000',
   withCredentials: true,
 });
