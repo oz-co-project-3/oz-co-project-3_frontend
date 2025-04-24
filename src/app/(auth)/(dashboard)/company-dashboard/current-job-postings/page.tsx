4e2ce5af93766a75f6333ffa4ca095025e30bc79
Nagi-ei <nakt4453@gmail.com>
2025-04-24 11:20:15 +0900

    ✨ 기업회원 대시보드 프로필 페이지 레이아웃 구현

--- src/app/(auth)/(dashboard)/company-dashboard/current-job-postings/page.tsx
+++ src/app/(auth)/(dashboard)/company-dashboard/current-job-postings/page.tsx
@@ -1,10 +1,16 @@
 import JobPostingCard from '@/components/dashboard/company/job-posting/JobPostingCard';
+import { Button } from '@/components/ui/button';
 
 export default async function CurrentJobPostingsPage() {
   return (
     <>
       <section className='flex flex-col gap-4 rounded-md bg-white px-4 py-10'>
-        <h2 className='border-b pb-4 text-2xl font-bold'>현재 채용공고</h2>
+        <div className='flex justify-between border-b pb-4'>
+          <h2 className='text-2xl font-bold'>현재 채용공고</h2>
+          <Button className='bg-main-light hover:bg-main-dark cursor-pointer'>
+            채용 공고 등록
+          </Button>
+        </div>
         <JobPostingCard />
         <JobPostingCard />
         <JobPostingCard />
--- src/app/(auth)/(dashboard)/company-dashboard/previous-job-postings/page.tsx
+++ src/app/(auth)/(dashboard)/company-dashboard/previous-job-postings/page.tsx
@@ -1,10 +1,17 @@
 import JobPostingCard from '@/components/dashboard/company/job-posting/JobPostingCard';
+import { Button } from '@/components/ui/button';
 
 export default async function PreviousJobPostingsPage() {
   return (
     <>
       <section className='flex flex-col gap-4 rounded-md bg-white px-4 py-10'>
-        <h2 className='border-b pb-4 text-2xl font-bold'>이전 채용공고</h2>
+        <div className='flex justify-between border-b pb-4'>
+          <h2 className='text-2xl font-bold'>이전 채용공고</h2>
+          <Button className='bg-main-light hover:bg-main-dark cursor-pointer'>
+            채용 공고 등록
+          </Button>
+        </div>
+
         <JobPostingCard />
         <JobPostingCard />
         <JobPostingCard />
@@ -15,3 +22,7 @@ export default async function PreviousJobPostingsPage() {
     </>
   );
 }
+
+// 버튼 내용 바꾸기
+// 마감일 연장 -> 재등록
+// 수정 -> 수정 후 재등록
