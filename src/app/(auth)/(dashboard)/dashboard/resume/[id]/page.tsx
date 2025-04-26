c7bd40da540cf312dac353d56e13326a4eed73bb
Nagi-ei <nakt4453@gmail.com>
2025-04-27 04:24:32 +0900

    ✨ 이력서 상세보기 페이지 구현

---
+++ src/app/(auth)/(dashboard)/dashboard/resume/[id]/page.tsx
@@ -0,0 +1,13 @@
+import Resume from '@/components/dashboard/seeker/resume/Resume';
+
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  console.log(id);
+
+  return (
+    <section className='flex flex-col gap-4 rounded-md bg-white px-4 py-10'>
+      <h2 className='border-b pb-4 text-2xl font-bold'>이력서 조회 (몇번째 칸인지 써주기?)</h2>
+      <Resume />
+    </section>
+  );
+}
