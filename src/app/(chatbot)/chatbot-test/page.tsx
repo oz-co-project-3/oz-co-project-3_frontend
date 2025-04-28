6df5acafa0de8eea3e6607e2250a3dbaa3f3bda3
minhui-p <alsgml1111@gmail.com>
2025-04-29 00:17:36 +0900

    ✨ Chatbot-test 페이지 async function 적용
    Chatbot-test 페이지 async function 적용

--- src/app/(chatbot)/chatbot-test/page.tsx
+++ src/app/(chatbot)/chatbot-test/page.tsx
@@ -1,6 +1,6 @@
 import ChatbotButton from '@/components/chatbot/ChatbotButton';
 
-export default function ChatbotPlayground() {
+export default async function ChatbotPlayground() {
   return (
     <main className='flex min-h-screen items-center justify-center bg-gray-100'>
       <h1 className='text-2xl text-gray-600'>챗봇 테스트 페이지</h1>
