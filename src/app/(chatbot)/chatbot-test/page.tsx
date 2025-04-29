b4390180cb432cd1ae6f585d5d1b4611ce23456b
Nagi-ei <nakt4453@gmail.com>
2025-04-29 15:54:41 +0900

    🧪 서버 컴포넌트와 클라이언트 컴포넌트의 환경 변수 차이점 적발
    찾았다 요놈

--- src/app/(chatbot)/chatbot-test/page.tsx
+++ src/app/(chatbot)/chatbot-test/page.tsx
@@ -1,6 +1,10 @@
 import ChatbotButton from '@/components/chatbot/ChatbotButton';
 
 export default async function ChatbotPlayground() {
+  console.log('WS:', process.env.NEXT_PUBLIC_WS_URL);
+  console.log('INTERNAL:', process.env.NEXT_PUBLIC_INTERNAL_BASE_URL);
+  console.log('EXTERNAL:', process.env.NEXT_PUBLIC_EXTERNAL_BASE_URL);
+
   return (
     <main className='flex min-h-screen items-center justify-center bg-gray-100'>
       <h1 className='text-2xl text-gray-600'>챗봇 테스트 페이지</h1>
--- src/app/(main)/private-jobs/page.tsx
+++ src/app/(main)/private-jobs/page.tsx
@@ -15,6 +15,8 @@ export default async function PrivateJobsPage({
   const resolvedSearchParams = await searchParams;
   const searchKeyword = resolvedSearchParams?.search_keyword;
 
+  console.log(process.env.NEXT_PUBLIC_INTERNAL_BASE_URL);
+
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_INTERNAL_BASE_URL}/api/postings/?employment_type=일반` +
       (searchKeyword ? `&search_keyword=${encodeURIComponent(searchKeyword)}` : ''),
