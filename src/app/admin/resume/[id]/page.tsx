f57ad2043f50fd4b3246a0a2213e7da756d66d28
minhui-p <alsgml1111@gmail.com>
2025-05-02 01:49:36 +0900

    🐛 타입 빌드오류 수정
    타입 빌드오류 수정

--- src/app/admin/resume/[id]/page.tsx
+++ src/app/admin/resume/[id]/page.tsx
@@ -2,17 +2,13 @@ import AdminLayout from '@/components/layout/AdminLayout';
 import DeleteResumeButton from '@/components/resume/DeleteResumeButton';
 import Resume from '@/components/resume/Resume';
 
-interface Props {
-  params: { id: string };
-}
-
-export default async function AdminResumeDetailPage({ params }: Props) {
+// 수정: Props 인터페이스 제거하고 params에 직접 타입 지정
+export default async function AdminResumeDetailPage({ params }: { params: { id: string } }) {
   const resumeId = params.id;
 
   return (
     <AdminLayout>
       <section className='flex flex-col gap-4 rounded-md bg-white px-8 py-10'>
-        {/* 버튼을 감싸서 너비 제어 */}
         <div className='flex items-center justify-between border-b pb-4'>
           <h2 className='text-2xl font-bold'>이력서 상세</h2>
           <div className='flex justify-end'>
