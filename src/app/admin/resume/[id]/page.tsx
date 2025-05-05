993a02144a7738b2487d4dbbbfdc59faea2bb8b5
Nagi-ei <nakt4453@gmail.com>
2025-05-05 19:07:35 +0900

    🐛 Resume 컴포넌트 변경에 의한 인자 전달 버그 수정

--- src/app/admin/resume/[id]/page.tsx
+++ src/app/admin/resume/[id]/page.tsx
@@ -19,7 +19,7 @@ export default function AdminResumeDetailPage() {
           </div>
         </div>
 
-        <Resume />
+        <Resume id={resumeId} />
       </section>
     </AdminLayout>
   );
