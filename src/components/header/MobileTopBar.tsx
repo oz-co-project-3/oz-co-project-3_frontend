97d76b4ad463333a15d89151819f28049d3c5fb7
Nagi-ei <nakt4453@gmail.com>
2025-05-15 10:58:34 +0900

    🐛 드롭다운 항목 클릭시 드롭다운 닫히게 수정

--- src/components/header/MobileTopBar.tsx
+++ src/components/header/MobileTopBar.tsx
@@ -1,8 +1,12 @@
 'use client';
 
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import Image from 'next/image';
+import useSWR from 'swr';
 import { ChevronLeft, Menu } from 'lucide-react';
 import { Button } from '../ui/button';
-import { useRouter } from 'next/navigation';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,10 +14,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuItem,
 } from '../ui/dropdown-menu';
-import Image from 'next/image';
-import Link from 'next/link';
 import { UserProfileResponse } from '@/types/user';
-import useSWR from 'swr';
 import { fetchOnClient } from '@/api/clientFetcher';
 import { useAuthStore } from '@/store/useAuthStore';
 import { businessNavItems, seekerNavItems } from '@/constants/nav';
@@ -22,6 +23,7 @@ import { logoutUser } from '@/api/user';
 export default function MobileTopBar() {
   const router = useRouter();
   const { user, logout } = useAuthStore();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const { data: profile } = useSWR<UserProfileResponse | null>(
     user ? '/api/user/profile/' : null,
@@ -65,20 +67,33 @@ export default function MobileTopBar() {
         </Link>
       </h1>
 
-      <DropdownMenu>
-        <DropdownMenuTrigger className='flex h-9 w-9 cursor-pointer items-center justify-center rounded-md border'>
-          <Menu />
+      <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
+        <DropdownMenuTrigger
+          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+          className='flex h-9 w-9 cursor-pointer items-center justify-center rounded-md border'
+        >
+          <Menu size={16} />
         </DropdownMenuTrigger>
         <DropdownMenuContent className='z-2000 -translate-x-2 translate-y-1 p-4'>
           {/* 로그아웃 상태 */}
           {!profile && (
             <>
-              <DropdownMenuItem className='cursor-pointer'>
+              <DropdownMenuItem
+                className='cursor-pointer'
+                onClick={() => {
+                  setIsDropdownOpen(false);
+                }}
+              >
                 <Link href='/user/login' className='p-1 text-base'>
                   로그인
                 </Link>
               </DropdownMenuItem>
-              <DropdownMenuItem className='cursor-pointer'>
+              <DropdownMenuItem
+                className='cursor-pointer'
+                onClick={() => {
+                  setIsDropdownOpen(false);
+                }}
+              >
                 <Link href='/user/register' className='p-1 text-base'>
                   회원가입
                 </Link>
@@ -89,7 +104,13 @@ export default function MobileTopBar() {
           {/* 로그인 상태 */}
           {profile &&
             seekerNavItems.map((item) => (
-              <DropdownMenuItem key={item.name} className='cursor-pointer'>
+              <DropdownMenuItem
+                key={item.name}
+                className='cursor-pointer'
+                onClick={() => {
+                  setIsDropdownOpen(false);
+                }}
+              >
                 <Link href={item.href} className='p-1 text-base'>
                   {item.name}
                 </Link>
@@ -101,7 +122,13 @@ export default function MobileTopBar() {
             <>
               <DropdownMenuSeparator />
               {businessNavItems.map((item) => (
-                <DropdownMenuItem key={item.name} className='cursor-pointer'>
+                <DropdownMenuItem
+                  key={item.name}
+                  className='cursor-pointer'
+                  onClick={() => {
+                    setIsDropdownOpen(false);
+                  }}
+                >
                   <Link href={item.href} className='p-1 text-base'>
                     {item.name}
                   </Link>
@@ -122,7 +149,12 @@ export default function MobileTopBar() {
           {profile && (
             <>
               <DropdownMenuSeparator />
-              <DropdownMenuItem className='p-0'>
+              <DropdownMenuItem
+                className='p-0'
+                onClick={() => {
+                  setIsDropdownOpen(false);
+                }}
+              >
                 <Link href='/' className='w-full'>
                   <Button
                     variant='outline'
